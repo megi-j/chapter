edacc380c8e95718454422359ffdb25d445bd818
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-03-07 12:27:25 +0400

    add menu

---
+++ main.js
@@ -0,0 +1,18 @@
+// var dropdownBtn = document.querySelector(".dropdown-btn");
+// var dropdownMenu = document.querySelector(".dropdown-menu");
+
+// dropdownBtn.addEventListener("click", function () {
+//   dropdownMenu.classList.toggle("show");
+// });
+
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropdown-btn")) {
+//     var dropdowns = document.getElementsByClassName("dropdown-menu");
+//     for (var i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
